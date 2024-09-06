b3aed738c4217fa8d9c694164eb53e4311fe86b7
Bhavya <107679448+BhavyaBabariya@users.noreply.github.com>
2024-09-06 11:16:28 +0530

    Update index.php

--- wp-content/themes/CustomTheme/index.php
+++ wp-content/themes/CustomTheme/index.php
@@ -1,1 +1,21 @@
+<?php
+get_header();
+?>
+    <div id="content">
+        <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+                the_title('<h2>', '</h2>');
+                the_content();
+            endwhile;
+        else :
+            echo '<p>No content found</p>';
+        endif;
+        ?>
+    </div>
+
+   <?php
+   get_footer();
+   ?>
+
 
